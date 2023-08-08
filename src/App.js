860380ec54637ddd3aec66154b4da2fddfed4b41
Adewale Adetoro <adetorodev19@gmail.com>
2023-08-08 23:19:35 +0100

    form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './useState/ClickCounter';
+import MyInput from './useState/TextField';
 
 function App() {
   return (
+    <>
     <Counter />
+    <MyInput />
+    <MyCheckbox />
+    <Form />
+    </>
   );
 }
 
