c03f6f79c9b4e70326ddb4fab008992c69c38b4f
Adewale Adetoro <adetorodev19@gmail.com>
2023-08-09 23:57:17 +0100

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import Counter from './useState/ClickCounter';
-import MyInput from './useState/TextField';
-import MyCheckbox from './useState/CheckBox';
-import Form from './useState/Form';
-import FormArray from './useState/FormArray';
-import NestedObject from './useState/NestedObject';
-import ListArray from './useState/ListArray';
-import BucketList from './useState/List';
+import ServerConnet from './useEffect/ConnectServer';
+
+import
 
 function App() {
   return (
     <>
-    <Counter />
-    <MyInput />
-    <MyCheckbox />
-    <Form />
-    {/* <FormArray /> */}
-    {/* <NestedObject /> */}
-    {/* <ListArray /> */}
-    {/* <BucketList /> */}
+      <ServerConnet />
     </>
   );
 }
+
 
 export default App;
